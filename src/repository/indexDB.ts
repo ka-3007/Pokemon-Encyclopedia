1c02e82bf7c078f2541a3f2862802b42510ea18c
yamada kaito <87706838+yamada-kaito@users.noreply.github.com>
2024-08-03 09:28:13 +0900

    indexDB修正

--- src/repository/indexDB.ts
+++ src/repository/indexDB.ts
@@ -1,5 +1,5 @@
 import { PokemonModel } from '@/model/pokemon';
-import { openDB, DBSchema } from 'idb';
+import { openDB, DBSchema, IDBPDatabase } from 'idb';
 
 // データベースのスキーマを定義
 interface PokemonDB extends DBSchema {
@@ -10,20 +10,25 @@ interface PokemonDB extends DBSchema {
 }
 
 // IndexedDBの初期化
-const dbPromise = openDB<PokemonDB>('pokemon-db', 1, {
-  upgrade(db) {
-    db.createObjectStore('pokemons');
-  },
-});
+let dbPromise: Promise<IDBPDatabase<PokemonDB>> | null = null;
+if (typeof window !== 'undefined') {
+  dbPromise = openDB<PokemonDB>('pokemon-db', 1, {
+    upgrade(db) {
+      db.createObjectStore('pokemons');
+    },
+  });
+}
 
 // データをIndexedDBに保存する関数
 export const saveToIndexedDB = async (pokemonData: PokemonModel) => {
+  if (!dbPromise) return undefined;
   const db = await dbPromise;
   await db.put('pokemons', pokemonData, pokemonData.name);
 };
 
 // IndexedDBからデータを取得する関数
 export const getFromIndexedDB = async (name: string): Promise<PokemonModel | undefined> => {
+  if (!dbPromise) return undefined;
   const db = await dbPromise;
   return db.get('pokemons', name);
 };
