205c162ec2ef0ec6f7558275d64ff8115e3c9814
yamada kaito <87706838+yamada-kaito@users.noreply.github.com>
2024-08-15 12:06:02 +0900

    apiから取得していたものを変更

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,5 +1,6 @@
 import { DetailContent } from '@/components/DetailContent';
 import { PokemonModel } from '@/model/pokemon';
+import { PokemonRepo } from '@/repository/pokemon';
 import { Suspense } from 'react';
 
 export default async function Detail({
@@ -9,13 +10,21 @@ export default async function Detail({
     name: string;
   };
 }) {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/fetchPokemonDetail?name=${searchParams.name}`, {
-    cache: 'no-store',
-  });
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/fetchPokemonDetail?name=${searchParams.name}`, {
+  //   cache: 'no-store',
+  // });
+  // const {
+  //   pokemonDetail,
+  //   newPokemonDetails,
+  // }: { pokemonDetail: PokemonModel; newPokemonDetails: Record<string, PokemonModel> } = await response.json();
+
   const {
     pokemonDetail,
     newPokemonDetails,
-  }: { pokemonDetail: PokemonModel; newPokemonDetails: Record<string, PokemonModel> } = await response.json();
+  }: { pokemonDetail: PokemonModel; newPokemonDetails: Record<string, PokemonModel> } = await fetchPokemonDetail(
+    searchParams.name,
+  );
+
   return (
     <Suspense
       fallback={
@@ -25,4 +34,41 @@ export default async function Detail({
       <DetailContent pokemonDetail={pokemonDetail} newPokemonDetails={newPokemonDetails} />
     </Suspense>
   );
+}
+
+async function fetchPokemonDetail(name: string) {
+  const pokemonDetail = (name && (await PokemonRepo.getPokemonDetail(name))) as PokemonModel;
+
+  const newPokemonDetails: Record<string, PokemonModel> = {};
+  if (pokemonDetail) {
+    if (pokemonDetail?.evolutionData || pokemonDetail?.formNames) {
+      const allPokemonNames = [
+        ...pokemonDetail?.evolutionData?.normalEvolution?.map((name) => name),
+        ...pokemonDetail?.evolutionData?.specialEvolution?.map((name) => name),
+      ];
+
+      for (const name of allPokemonNames) {
+        if (name !== pokemonDetail?.name) {
+          const pokemon = await PokemonRepo.getPokemonDetail(name);
+          if (pokemon) {
+            newPokemonDetails[name] = pokemon;
+          }
+        }
+      }
+
+      if (pokemonDetail?.formNames) {
+        // フォーム違いのポケモン詳細を取得
+        for (const name of pokemonDetail?.formNames) {
+          if (name !== pokemonDetail?.name) {
+            const pokemon = await PokemonRepo.getPokemonDetail(name);
+            if (pokemon) {
+              newPokemonDetails[name] = pokemon;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return { pokemonDetail, newPokemonDetails };
 }
